082bbdfea4571b306eaff8142f841fe31085bf66
Hitesh-Dingrai <91075197+Hitesh-Dingrai@users.noreply.github.com>
2022-01-22 13:36:19 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,24 @@
+
+#add
+def add(x,y):
+     return x+y
+     
+     
+#subtract
+def subtract(x,y):
+     return x-y
+ #divide    
+def divide(x,y):
+
+     if y=0:
+        return divide_by_0_error
+     else:
+        return x/y
+
+ 
+#multiply
+def multiply(x,y):
+     return x-y
+     
+def square(x):
+    pass
